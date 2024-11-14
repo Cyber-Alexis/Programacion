d6136d6dd9373a242f829ebcfd3244564b32cc3c
Cyber-Alexis <alexis.n.buza@alumnes.ilerna.com>
2024-11-14 18:58:19 +0100

    Texto

---
+++ src/Ejercicios/El_Juego_del_Colgado.java
@@ -0,0 +1,11 @@
+package Ejercicios;
+
+public class El_Juego_del_Colgado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Hola este sera un ejercicio
+	}
+
+}
